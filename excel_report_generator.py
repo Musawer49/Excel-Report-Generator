14d3604f0debbc423df8d554215d0ed9c4baf110
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-09-22 23:46:48 +0500

    Generate sales report

--- excel_report_generator.py
+++ excel_report_generator.py
@@ -1,6 +1,14 @@
 import pandas as pd
 data = pd.read_excel("sales_data.xlsx")
 print(data)
-data["Total"] = data["Quantity"] = data["Price"]
+data["Total"] = data["Quantity"] * data["Price"]
 total_sales = data["Total"].sum()
-print("Total Sales:", total_sales)
+#print("Total Sales:", total_sales)
+category_sales = data.groupby("Category")["Total"].sum()
+# print("\nSales by Category: ", category_sales)
+product_sales = data.groupby("Product")["Total"].sum()
+top_product = product_sales.idxmax()
+# print("\nTop-Selling Product: ", top_product)
+report = data.copy()
+report.to_excel("sales_report.xlsx", index=False)
+print("\nReport generated successfully!")
