75f9b6c4d308a717a1b9ca8cdf790ce9a73b700e
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-09-23 23:39:33 +0500

    Add total quantity sold to rport

--- excel_report_generator.py
+++ excel_report_generator.py
@@ -1,14 +1,24 @@
+print("NEW VERSION RUNNING")
 import pandas as pd
 data = pd.read_excel("sales_data.xlsx")
 print(data)
 data["Total"] = data["Quantity"] * data["Price"]
 total_sales = data["Total"].sum()
-#print("Total Sales:", total_sales)
+
 category_sales = data.groupby("Category")["Total"].sum()
-# print("\nSales by Category: ", category_sales)
+
 product_sales = data.groupby("Product")["Total"].sum()
 top_product = product_sales.idxmax()
-# print("\nTop-Selling Product: ", top_product)
-report = data.copy()
+
+total_quantity_sales = data["Quantity"].sum()
+print(total_quantity_sales)
+
+report = pd.DataFrame(
+    [
+        ["Total Quantity Sold", total_quantity_sales],
+        ["Total Sales", total_sales],
+        ["Top Productt", top_product],
+    ],
+    columns=["Sales", "Values"]
+)
 report.to_excel("sales_report.xlsx", index=False)
-print("\nReport generated successfully!")
